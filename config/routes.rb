b1c877a0523216ec87efdd90b92dab0d87add3d7
Daniel Haro <dannharo@hotmail.com>
2017-05-21 03:29:44 -0500

    ready to deploy in heroku

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'magma/my_sport'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'magma#about_me'
 end
